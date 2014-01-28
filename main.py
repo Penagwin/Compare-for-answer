00445a0018cb8eb5326125f6391035ef0dacb4f8
Penagwin <langpenguin@gmail.com>
2014-01-28 15:23:07 -0500

    deployment

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print "Started"
 answers = open('/home/penagwin/.config/sublime-text-3/Packages/User/Question.txt', 'r')
 questions = open('/home/penagwin/.config/sublime-text-3/Packages/User/Question', 'r')
 for question in questions:
-	if not question == "\n":
+	if question != "\n":
 		for answer in answers:
 			if answer.find("Core Democratic Values") != -1:
 				print answer
