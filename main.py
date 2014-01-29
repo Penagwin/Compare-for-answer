1d24f75f16d9736bac5838a929618a857ce5fe4a
Penagwin <langpenguin@gmail.com>
2014-01-28 21:05:44 -0500

    deployment

--- main.py
+++ main.py
@@ -7,44 +7,32 @@ answeryet = False
 nextline = False
 pos = []
 small = 0
-answerb = False
+answerb = 0
 answers = open('/home/penagwin/.config/sublime-text-3/Packages/User/Question.txt', 'r')
 questions = open('/home/penagwin/.config/sublime-text-3/Packages/User/Answers.txt', 'r')
 for question in questions:
 	for answer in answers:
 		pos.append(answers.tell())
 		currentquestion = question.replace("\n", "")
-		if answer.find("Correct Answer:") != -1:
-			answerb = True
 		if answer.find("Question") != -1:
-			answerb = False 
-			nextline = True
-		if answer.find(currentquestion) != -1:
-			answeryet = True
-		if answer.find(currentquestion) != -1 and answer == True:
-			while pos.index(len(pos)-small) != "Answer":
-				small = small + 1
-				answers.seek(pos.index(len(pos)-small))
-
-			answers.seek(pos.index(len(pos)-small-1))
+			answerb = 1 
+		elif answer.find("Correct Answer:") != -1:
+			answerb = 2
+		
+		if answer.find(currentquestion) != -1 and answerb == 1:
+			print answer
+		if answer.find(currentquestion) != -1 and answerb == 2:
+			answers.seek(pos[len(pos)-1])
+			while answer.find("Answer\n") != -1:
+				small = small +1
+				answers.seek(pos[len(pos)-1])
+			answers.seek(pos[len(pos)-small-1])
 			print answers
-			answers.seek(pos.index(len(pos)))
+			answers.seek(pos[len(pos)-1])
 			small=0
-
-		if nextline == True and answer != "\n":
-			print question
-			print answer + "\n \n"
-			nextline = False
-			answeryet = False
-		
-		
-
-		
-
-
+			answerb = False
 	answers.seek(0)
-	nextline = False
-	answeryet = False
+	answerb = 0
 
 answers.close()
 questions.close()
