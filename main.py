604b08fc3934268819aa548c72b9bfe09f803e4b
Penagwin <langpenguin@gmail.com>
2014-01-28 21:40:28 -0500

    deployment

--- main.py
+++ main.py
@@ -24,10 +24,15 @@ for question in questions:
 		
 		if answer.find(currentquestion) != -1 and answerb == 1:
 			print answer
+			answer = answers.readline()
+			while answer == "/n":
+				answer = answers.readline()
+
 		if answer.find(currentquestion) != -1 and answerb == 2:
+			print answer
 			while answer.lower().find("answer") == -1 or answer.lower().find("answer:") != -1:
 				small = small +1
-				print " "+str(len(pos)-small)+ "        " + str(len(pos)) + '                '+ answers.readline()
+				#print " "+str(len(pos)-small)+ "        " + str(len(pos)) + '                '+ answers.readline()
 				answers.seek(pos[len(pos)-small])
 				answer = answers.readline()
 				answers.seek(pos[len(pos)-small-1])
@@ -35,8 +40,6 @@ for question in questions:
 			answers.seek(pos[len(pos)-1])
 			small=0
 			answerb = 0
-		else:
-			print "\n"
 	answers.seek(0)
 	answerb = 0
 
