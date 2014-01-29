6ab135fd98339417b0002c4f1caae4e52e99f7fc
Penagwin <langpenguin@gmail.com>
2014-01-28 21:49:50 -0500

    deployment

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ for question in questions:
 		elif answer.find("Correct Answer:") != -1:
 			answerb = 2
 		
-		if answer.find(currentquestion) != -1 and answerb != 2:
+		if answer.find(currentquestion) != -1 and answerb == 0:
 			print answer
 
 		if answer.find(currentquestion) != -1 and answerb == 2:
