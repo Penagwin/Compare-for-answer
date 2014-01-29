52de064d3e0144287c43e0d8141725a96b581ab2
Penagwin <langpenguin@gmail.com>
2014-01-28 21:38:31 -0500

    deployment

--- main.py
+++ main.py
@@ -24,17 +24,19 @@ for question in questions:
 		
 		if answer.find(currentquestion) != -1 and answerb == 1:
 			print answer
-		if answer.find(currentquestion) != -1 and answerb == 2:
+		elif answer.find(currentquestion) != -1 and answerb == 2:
 			while answer.lower().find("answer") == -1 or answer.lower().find("answer:") != -1:
 				small = small +1
 				print " "+str(len(pos)-small)+ "        " + str(len(pos)) + '                '+ answers.readline()
 				answers.seek(pos[len(pos)-small])
 				answer = answers.readline()
 				answers.seek(pos[len(pos)-small-1])
-			print "ummm" +answers.readline()
+			print answers.readline()
 			answers.seek(pos[len(pos)-1])
 			small=0
 			answerb = 0
+		else:
+			print "\n"
 	answers.seek(0)
 	answerb = 0
 
